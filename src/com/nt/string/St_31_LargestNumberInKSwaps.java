99d737bac36914c5feb7c452a51f199b058284fe
adityarajlsipl <sksaifuddin575@gmail.com>
2023-03-18 10:26:33 +0530

    Largest Number Function

--- src/com/nt/string/St_31_LargestNumberInKSwaps.java
+++ src/com/nt/string/St_31_LargestNumberInKSwaps.java
@@ -15,10 +15,48 @@ public class St_31_LargestNumberInKSwaps {
 		return s1;
 	}
 
+	static String max;
+
+	// O(n!/(n-k)!) , where n = length of input string and S(C)=O(n)
+	public static String findMaximumNum(String str, int k) {
+		max = str;
+		findMaximum(str.toCharArray(), k);
+		return max;
+	}
+
+	public static void findMaximum(char[] strArr, int k) {
+		if (k == 0)
+			return;
+
+		for (int i = 0; i < strArr.length - 1; i++) {
+			for (int j = i + 1; j < strArr.length; j++) {
+				if (strArr[j] > strArr[i]) {
+					strArr = swap(strArr, i, j);
+					String st = new String(strArr);
+					if (max.compareTo(st) < 0)
+						max = st;
+					findMaximum(strArr, k - 1);
+					strArr = swap(strArr, i, j);
+				}
+			}
+		}
+	}
+
+	public static char[] swap(char[] strArr, int i, int j) {
+		char temp = strArr[i];
+		strArr[i] = strArr[j];
+		strArr[j] = temp;
+		return strArr;
+	}
+
 	public static void main(String[] args) {
 		String str = "9002063118";
 		String res = largestNumber(str);
 		System.out.println("Largest Number is::" + res);
+
+		String input = "1234567";
+		String output = findMaximumNum(input, 3);
+		System.out.println("Maximum Large Number is::" + output);
 
 	}
 
