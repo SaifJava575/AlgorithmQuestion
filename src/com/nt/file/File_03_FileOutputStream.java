8d561e592d4622feaf560b15b71c5ca1e76467e6
Saifuddin575408 <sksaifuddin575@gmail.com>
2023-03-28 17:59:14 +0530

    File

---
+++ src/com/nt/file/File_03_FileOutputStream.java
@@ -0,0 +1,13 @@
+package com.nt.file;
+
+public class File_03_FileOutputStream {
+	
+	public void fileOutPut(String file) {
+		
+	}
+
+	public static void main(String[] args) {
+		String txt="D:\\SaifuddinWorkSpace\\CompressFile\\info.txt";
+	}
+
+}
