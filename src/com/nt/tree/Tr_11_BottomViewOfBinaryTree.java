f093afe187e99a810666c3cd48eaa572e19b2bb5
SaifJava <saifuddinkurumba@gmail.com>
2023-07-07 10:57:55 +0530

    Bottom view of Binary Tree and Sum path of Two Special Nodes

--- src/com/nt/tree/Tr_11_BottomViewOfBinaryTree.java
+++ src/com/nt/tree/Tr_11_BottomViewOfBinaryTree.java
@@ -6,11 +6,10 @@ import java.util.Map;
 import java.util.Queue;
 import java.util.TreeMap;
 
+public class Tr_11_BottomViewOfBinaryTree {
 
-class Tr_07_TopViewOfBinaryTree {
-
-	// T(C)=O(N) and S(c)=O(N)
-	static ArrayList<Integer> topView(Node root) {
+	//T(C)=O(N*logN).  and S(c)=O(n)
+	public static ArrayList<Integer> bottomView(Node root) {
 		ArrayList<Integer> ans = new ArrayList<>();
 		if (root == null)
 			return ans;
@@ -21,8 +20,8 @@ class Tr_07_TopViewOfBinaryTree {
 			Pair it = q.poll();
 			Node temp = it.node;
 			int hd = it.level;
-			if (map.get(hd) == null)
-				map.put(hd, temp.data);
+			//if (map.get(hd) == null)
+			map.put(hd, temp.data);
 			if (temp.left != null)
 				q.add(new Pair(temp.left, hd - 1));
 			if (temp.right != null)
@@ -34,19 +33,19 @@ class Tr_07_TopViewOfBinaryTree {
 	}
 
 	public static void main(String[] args) {
-    Node root=new Node(1);
-    root.left=new Node(2);
-    root.right=new Node(3);
-    
-    root.left.left=new Node(4);
-    root.left.right=new Node(5);
-    
-    root.right.left=new Node(6);
-    root.right.right=new Node(7);
-    
-    ArrayList<Integer> response=topView(root);
-    System.out.println("Top View of Binary Tree is ::"+response);
-    
+		Node root = new Node(1);
+		root.left = new Node(2);
+		root.right = new Node(3);
+
+		root.left.left = new Node(4);
+		root.left.right = new Node(5);
+
+		root.right.left = new Node(6);
+		root.right.right = new Node(7);
+		
+		ArrayList<Integer> response=bottomView(root);
+		System.out.println("The bottom view of the Binary Tree is ::"+response);
+
 	}
 
 }
