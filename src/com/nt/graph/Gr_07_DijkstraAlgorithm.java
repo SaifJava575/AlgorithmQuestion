badb70a3300c358e9e9a18d26f1b73234f7c2cbc
SaifJava <saifuddinkurumba@gmail.com>
2023-07-19 18:04:53 +0530

    Bi Partial Graph

---
+++ src/com/nt/graph/Gr_07_DijkstraAlgorithm.java
@@ -0,0 +1,16 @@
+package com.nt.graph;
+
+import java.util.ArrayList;
+
+public class Gr_07_DijkstraAlgorithm {
+
+	static int[] dijkstra(int V, ArrayList<ArrayList<ArrayList<Integer>>> adj, int S) {
+		int ans[]=new int[V];
+		return ans;
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
